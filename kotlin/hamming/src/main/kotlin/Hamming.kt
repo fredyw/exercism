5ef29ea3e3a12bf140942b5bf0bbdcb6d0b4ad19
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-09 16:46:29 -0500

    Solve hamming problem

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,9 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        require(leftStrand.length == rightStrand.length) {
+            "left and right strands must be of equal length"
+        }
+        return leftStrand.zip(rightStrand).count { (c1, c2) -> c1 != c2 }
     }
 }
