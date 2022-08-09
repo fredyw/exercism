3d99e3a5e4d33c861adb481761585ab57769911a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-09 17:00:42 -0500

    Solve gigasecond problem

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,8 +1,8 @@
+import java.time.LocalDate
 import java.time.LocalDateTime
 
-class Gigasecond {
+class Gigasecond(dateTime: LocalDateTime) {
+    constructor(date: LocalDate) : this(date.atStartOfDay())
 
-    // TODO: Implement proper constructor
-
-    val date: LocalDateTime = TODO("Implement this getter to complete the task")
+    val date: LocalDateTime = dateTime.plusSeconds(1_000_000_000)
 }
