9582633e1f5bdd5a92f2cd01451d800739811966
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-13 19:36:57 -0500

    Solve triangle problem.

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,8 +1,16 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
+    init {
+        val x = a.toDouble()
+        val y = b.toDouble()
+        val z = c.toDouble()
+        require(x > 0 && y > 0 && z > 0)
+        val list = mutableListOf(x, y, z).apply {
+            sort()
+        }
+        require(list[0] + list[1] >= list[2])
+    }
 
-    // TODO: Implement proper constructor
-
-    val isEquilateral: Boolean = TODO("Implement this getter to complete the task")
-    val isIsosceles: Boolean = TODO("Implement this getter to complete the task")
-    val isScalene: Boolean = TODO("Implement this getter to complete the task")
+    val isEquilateral: Boolean = a == b && b == c
+    val isIsosceles: Boolean = a == b || b == c || a == c
+    val isScalene: Boolean = a != b && b != c && a != c
 }
