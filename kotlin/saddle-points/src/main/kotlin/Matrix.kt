88989da749bd23977a764065f15d6e18baf3007e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-19 17:06:03 -0500

    Solve saddle-points problem

--- kotlin/saddle-points/src/main/kotlin/Matrix.kt
+++ kotlin/saddle-points/src/main/kotlin/Matrix.kt
@@ -1,1 +1,18 @@
 data class MatrixCoordinate(val row: Int, val col: Int)
+data class Matrix(val matrix: List<List<Int>>) {
+    val saddlePoints: Set<MatrixCoordinate> get() {
+        val colSize = if (matrix.isNotEmpty()) matrix[0].size else 0
+        val maxRows = matrix.map { it.maxOrNull() ?: Int.MIN_VALUE }
+        val minCols = (0 until colSize).map { colIndex -> matrix.map { it[colIndex] } }
+            .map { it.minOrNull() ?: Int.MAX_VALUE }
+        return matrix.flatMapIndexed { rowIndex, col ->
+            col.mapIndexedNotNull { colIndex, num ->
+                if (num == maxRows[rowIndex] && num == minCols[colIndex]) {
+                    MatrixCoordinate(rowIndex + 1, colIndex + 1)
+                } else {
+                    null
+                }
+            }
+        }.toSet()
+    }
+}
