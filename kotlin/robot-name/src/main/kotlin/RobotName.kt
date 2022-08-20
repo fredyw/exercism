39d0607baab36c2e8c998316435db8ac11691ebf
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-20 10:13:15 -0500

    Solve robot-name problem

--- kotlin/robot-name/src/main/kotlin/RobotName.kt
+++ kotlin/robot-name/src/main/kotlin/RobotName.kt
@@ -1,9 +1,32 @@
+import kotlin.random.Random
+
 class Robot {
+    private var generatedName = generate()
 
     val name: String
-        get() = TODO("Implement this getter to complete the task")
+        get() = generatedName
 
     fun reset() {
-        TODO("Implement this function to complete the task")
+        generatedName = generate()
+    }
+
+    companion object {
+        private val USED_NAMES = mutableSetOf<String>()
+
+        private fun generate(): String {
+            val n = Random.nextInt(0, 26 * 26 * 10 * 10 * 10)
+            var name = makeString(n)
+            while (name in USED_NAMES) {
+                val n = Random.nextInt(0, 26 * 26 * 10 * 10 * 10)
+                name = makeString(n)
+            }
+            USED_NAMES += name
+            return name
+        }
+        private fun makeString(n: Int) = "${'A' + (n / 26000 % 26)}" +
+                "${'A' + (n / 1000 % 26)}" +
+                "${n / 100 % 10}" +
+                "${n / 10 % 10}" +
+                "${n % 10}"
     }
 }
