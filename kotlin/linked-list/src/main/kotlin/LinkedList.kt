6678d0358bf493a9bce254c8e8f5553aafc17669
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-25 20:29:32 -0500

    Solve linked-list problem

--- kotlin/linked-list/src/main/kotlin/LinkedList.kt
+++ kotlin/linked-list/src/main/kotlin/LinkedList.kt
@@ -1,18 +1,57 @@
 class Deque<T> {
+    data class Node<T>(
+        val value: T,
+        var next: Node<T>? = null,
+        var prev: Node<T>? = null,
+    )
+
+    // tail <---> head
+    var head: Node<T>? = null
+    var tail: Node<T>? = null
 
     fun push(value: T) {
-        TODO("Implement this function to complete the task")
+        if (head == null) {
+            val node = Node(value)
+            head = node
+            tail = node
+        } else {
+            val node = Node(value)
+            node.prev = head
+            head?.next = node
+            head = head?.next
+        }
     }
 
     fun pop(): T? {
-        TODO("Implement this function to complete the task")
+        val value = head?.value
+        head = head?.prev
+        if (head == null) {
+            tail = null
+        }
+        head?.next = null
+        return value
     }
 
     fun unshift(value: T) {
-        TODO("Implement this function to complete the task")
+        if (tail == null) {
+            val node = Node(value)
+            tail = node
+            head = node
+        } else {
+            val node = Node(value)
+            node.next = tail
+            tail?.prev = node
+            tail = tail?.prev
+        }
     }
 
     fun shift(): T? {
-        TODO("Implement this function to complete the task")
+        val value = tail?.value
+        tail = tail?.next
+        if (tail == null) {
+            head = null
+        }
+        tail?.prev = null
+        return value
     }
 }
