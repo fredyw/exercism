81e65e371beac57fe6abb93386226478255187d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-27 10:02:10 -0500

    Solve binary-search problem

--- kotlin/binary-search/src/main/kotlin/BinarySearch.kt
+++ kotlin/binary-search/src/main/kotlin/BinarySearch.kt
@@ -1,5 +1,18 @@
 object BinarySearch {
     fun search(list: List<Int>, item: Int): Int {
-        TODO("Implement the function to complete the task. Change the signature if necessary.")
+        var l = 0
+        var r = list.size - 1
+        while (l <= r) {
+            val mid = (l + r) / 2
+            if (item == list[mid]) {
+                return mid
+            }
+            if (item < list[mid]) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        throw NoSuchElementException()
     }
 }
