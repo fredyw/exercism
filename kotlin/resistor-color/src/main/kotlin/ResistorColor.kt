32ff24fba26636aa52b7bc1b6fc8fb1a5e524f6f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-30 09:27:55 -0500

    Solve resistor-color problem

--- kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
+++ kotlin/resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,9 +1,22 @@
 object ResistorColor {
     fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
+        return COLOR_MAP[input] ?: throw IllegalArgumentException()
     }
 
     fun colors(): List<String> {
-        TODO("Implement this to complete the task")
+        return COLOR_MAP.keys.toList()
     }
+
+    private val COLOR_MAP = mapOf(
+        "black" to 0,
+        "brown" to 1,
+        "red" to 2,
+        "orange" to 3,
+        "yellow" to 4,
+        "green" to 5,
+        "blue" to 6,
+        "violet" to 7,
+        "grey" to 8,
+        "white" to 9,
+    )
 }
