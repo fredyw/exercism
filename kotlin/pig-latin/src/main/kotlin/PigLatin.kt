22c29806c50cfe4ef9a085eef950ff103d5e8807
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-30 00:33:45 -0500

    Solve pig-latin problem

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,6 +1,18 @@
 object PigLatin {
+    private val VOWEL_RULE = "(?:[aeiou]|yr|xr|yt).*".toRegex()
+    private val CONSONANT_RULE =
+        "(thr?|sch|ch|s?qu|rh|[bcdfghjklmnpqrstvwxyz])(.*)".toRegex()
 
     fun translate(phrase: String): String {
-        TODO("Implement this function to complete the task")
+        return phrase.split(" ").joinToString(" ") {
+            when {
+                it.matches(VOWEL_RULE) -> "${it}ay"
+                it.matches(CONSONANT_RULE) -> CONSONANT_RULE.replace(
+                    it,
+                    "$2$1ay"
+                )
+                else -> it
+            }
+        }
     }
 }
