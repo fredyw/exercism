043ff93647e7e09d9c972f91caf7786c7793d73d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-03 00:31:39 -0500

    Solve diamond problem

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,5 +1,18 @@
 class DiamondPrinter {
     fun printToList(char: Char): List<String> {
-        TODO()
+        val n = char - 'A'
+        return listOf((0..n), (n - 1 downTo 0)).flatMap { list ->
+            list.map { printLine(n - it, (it + 'A'.code).toChar()) }
+        }
+    }
+
+    private fun printLine(n: Int, char: Char): String {
+        return if (char == 'A') {
+            "${" ".repeat(n)}$char${" ".repeat(n)}"
+        } else {
+            val gap = (char.code - 'A'.code) + (char.code - 'A'.code - 1)
+            "${" ".repeat(n)}$char${" ".repeat(gap)}$char${" ".repeat(n)}"
+        }
     }
 }
+
