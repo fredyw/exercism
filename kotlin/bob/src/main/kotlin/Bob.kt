70fff8b1ff5496f11c788c5ecaf6116916818549
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-04 16:23:48 -0500

    Solve bob problem

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,17 @@
 object Bob {
     fun hey(input: String): String {
-        TODO("Implement the function to complete the task")
+        fun isYelling(): Boolean {
+            val letters = input.trim().filter { it.isLetter() }
+            return if (letters.isNotEmpty()) letters.all { it.isUpperCase() } else false
+        }
+
+        return when {
+            input.trim().isEmpty() -> "Fine. Be that way!"
+            isYelling() && input.endsWith("?") -> "Calm down, I know what I'm doing!"
+            isYelling() -> "Whoa, chill out!"
+            input.trim().endsWith("?") -> "Sure."
+            else -> "Whatever."
+        }
     }
 }
+
