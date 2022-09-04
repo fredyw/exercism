ef808ef15c7057e1334ed78233284d7b1de81d54
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-04 16:51:26 -0500

    Solve beer-song problem

--- kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -1,5 +1,23 @@
 object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int){
-        TODO("Implement the function to complete the task")
+    fun verses(startBottles: Int, takeDown: Int): String {
+        fun bottles(n: Int) = when (n) {
+            0 -> "no more bottles"
+            1 -> "$n bottle"
+            else -> "$n bottles"
+        }
+
+        return (startBottles downTo takeDown).joinToString("\n") {
+            if (it == 0) {
+                "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+            } else {
+                "${bottles(it)} of beer on the wall, ${bottles(it)} of beer.\n" +
+                        "Take ${if (it == 1) "it" else "one"} down and pass it around, ${
+                            bottles(
+                                it - 1
+                            )
+                        } of beer on the wall.\n"
+            }
+        }
     }
 }
