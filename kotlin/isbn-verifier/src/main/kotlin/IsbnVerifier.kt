814c5afe939d64af1b5890d7f0a5c0de3b77a534
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-06 09:42:37 -0500

    Solve isbn-verifier problem

--- kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
+++ kotlin/isbn-verifier/src/main/kotlin/IsbnVerifier.kt
@@ -1,6 +1,18 @@
 class IsbnVerifier {
-
     fun isValid(number: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val num = number.replace("-", "")
+        if (num.length != 10) {
+            return false
+        }
+        if (!num.take(9).all { it.isDigit() }) {
+            return false
+        }
+        if (!num[9].isDigit() && num[9] != 'X') {
+            return false
+        }
+        return (10 downTo 1).zip(num.asIterable())
+            .sumOf { (i, c) ->
+                i * if (c == 'X') 10 else c.digitToInt()
+            } % 11 == 0
     }
 }
