ab7b898bee147d6a2f71ab5e57097eecb6363f1f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-06 10:10:42 -0500

    Update largest-series-product problem

--- kotlin/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
+++ kotlin/largest-series-product/src/main/kotlin/LargestSeriesProduct.kt
@@ -1,8 +1,10 @@
-class Series {
-
-    // TODO: Implement proper constructor
+class Series(private val num: String) {
+    init {
+        require(num.all { it.isDigit() })
+    }
 
     fun getLargestProduct(span: Int): Long {
+        require(num.length >= span && span >= 0)
         TODO("Implement this function to complete the task")
     }
 }
