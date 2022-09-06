d04fff79aacc161758231575672aed106498fa90
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-06 13:57:21 -0500

    Solve luhn problem

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,6 +1,27 @@
 object Luhn {
-
     fun isValid(candidate: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val s = candidate.replace("\\s+".toRegex(), "")
+        if (s.length <= 1) {
+            return false
+        }
+        if (!s.all { it.isDigit() }) {
+            return false
+        }
+        return (s.indices).sumOf { i ->
+            val index = s.lastIndex - i
+            s[index].digitToInt().let { digit ->
+                if (i % 2 == 1) {
+                    (digit * 2).let { doubled ->
+                        if (doubled > 9) {
+                            doubled - 9
+                        } else {
+                            doubled
+                        }
+                    }
+                } else {
+                    digit
+                }
+            }
+        } % 10 == 0
     }
 }
