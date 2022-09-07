a02147ff5393e84f888b143637247b3a3f09cca3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-07 15:53:20 -0500

    Solve phone-number problem

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,6 +1,11 @@
-class PhoneNumber {
-
-    // TODO: Implement proper constructor
-
-    val number: String? = TODO("Implement this getter to complete the task")
+class PhoneNumber(private val phoneNumber: String) {
+    val number: String? =
+        phoneNumber.filter { it.isDigit() }.let {
+            require(it.matches("1?[2-9]\\d{2}[2-9]\\d{6}".toRegex()))
+            if (it.length == 11) {
+                it.substring(1, it.length)
+            } else {
+                it
+            }
+        }
 }
