9454700f9e3d5f8a8cde1f7527700ac9edfff3a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-25 14:50:46 -0500

    Add meetup problem

---
+++ kotlin/meetup/src/main/kotlin/Meetup.kt
@@ -0,0 +1,8 @@
+import java.time.DayOfWeek
+import java.time.LocalDate
+
+class Meetup(private val month: Int, private val year: Int) {
+    fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
+        TODO("Implement this function to complete the task")
+    }
+}
