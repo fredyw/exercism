901a0a9f3b30c99a888cf7940184cd8dfd69f11a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-26 20:14:38 -0500

    Solve meetup problem

--- kotlin/meetup/src/main/kotlin/Meetup.kt
+++ kotlin/meetup/src/main/kotlin/Meetup.kt
@@ -2,7 +2,21 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 
 class Meetup(private val month: Int, private val year: Int) {
+    private val dates: List<LocalDate> =
+        generateSequence(LocalDate.of(year, month, 1)) { date ->
+            date.plusDays(1).let { if (date.month == it.month) it else null }
+        }.toList()
+
     fun day(dayOfWeek: DayOfWeek, schedule: MeetupSchedule): LocalDate{
-        TODO("Implement this function to complete the task")
+        return dates.filter { it.dayOfWeek == dayOfWeek }.let { date ->
+            when (schedule) {
+                MeetupSchedule.FIRST -> date.first()
+                MeetupSchedule.SECOND -> date.drop(1).first()
+                MeetupSchedule.THIRD -> date.drop(2).first()
+                MeetupSchedule.FOURTH -> date.drop(3).first()
+                MeetupSchedule.LAST -> date.last()
+                MeetupSchedule.TEENTH -> date.find { it.dayOfMonth > 12 }!!
+            }
+        }
     }
 }
