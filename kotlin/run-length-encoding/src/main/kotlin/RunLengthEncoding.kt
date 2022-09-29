523f4996c32213136aa51efc96012b57e63ddb30
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-29 00:22:06 -0500

    Solve run-lenght-encoding problem

--- kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
+++ kotlin/run-length-encoding/src/main/kotlin/RunLengthEncoding.kt
@@ -1,10 +1,17 @@
 object RunLengthEncoding {
-
     fun encode(input: String): String {
-        TODO("Implement this to complete the task")
+        return input.replace("(.)\\1+".toRegex()) {
+            if (it.value.length == 1) {
+                it.groupValues[1]
+            } else {
+                "${it.value.length}${it.groupValues[1]}"
+            }
+        }
     }
 
     fun decode(input: String): String {
-        TODO("Implement this to complete the task")
+        return input.replace("(\\d+)(.)".toRegex()) {
+            it.groupValues[2].repeat(it.groupValues[1].toInt())
+        }
     }
 }
