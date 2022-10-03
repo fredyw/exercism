176744aa3222ba9d463fe190b0431696e11cda18
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-03 00:26:57 -0500

    Solve scale-generator problem

--- kotlin/scale-generator/src/main/kotlin/Scale.kt
+++ kotlin/scale-generator/src/main/kotlin/Scale.kt
@@ -1,11 +1,36 @@
 class Scale(private val tonic: String) {
+    private val scaleIndex: Int =
+        SCALE_TO_INDEX_MAP[tonic.uppercase()]
+            ?: SCALE_TO_INDEX_MAP[FLAT_TO_SHARP_MAP[tonic.uppercase()]]!!
 
     fun chromatic(): List<String> {
-        TODO("Implement this function to complete the task")
+        return SCALES.indices.map { SCALES[(it + scaleIndex) % SCALES.size] }
     }
 
     fun interval(intervals: String): List<String> {
-        TODO("Implement this function to complete the task")
+        return intervals.dropLast(1).scan(scaleIndex) { index, char ->
+            when (char) {
+                'm' -> index + 1
+                'M' -> index + 2
+                else -> index + 3
+            } % SCALES.size
+        }.map { SCALES[it] }
     }
 
+    companion object {
+        private val SCALES = listOf(
+            "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"
+        )
+
+        val SCALE_TO_INDEX_MAP =
+            SCALES.withIndex().associate { (index, scale) -> scale to index }
+
+        private val FLAT_TO_SHARP_MAP = mapOf(
+            "BB" to "A#",
+            "DB" to "C#",
+            "EB" to "D#",
+            "GB" to "F#",
+            "AB" to "G#",
+        )
+    }
 }
