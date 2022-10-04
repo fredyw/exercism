fa6f7e4bb06961888995762aeaf24368e3dec2f7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-03 23:56:36 -0500

    Solve perfect-numbers problem

--- kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
+++ kotlin/perfect-numbers/src/main/kotlin/NaturalNumber.kt
@@ -1,8 +1,16 @@
-
 enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
 fun classify(naturalNumber: Int): Classification {
-    return Classification.DEFICIENT
+    require(naturalNumber > 0)
+    val aliquotSum =
+        (1..naturalNumber / 2)
+            .filter { naturalNumber % it == 0 }
+            .sum()
+    return when {
+        aliquotSum == naturalNumber -> Classification.PERFECT
+        aliquotSum > naturalNumber -> Classification.ABUNDANT
+        else -> Classification.DEFICIENT
+    }
 }
