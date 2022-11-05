6a6e777634a4ca804b7f6f20bfcac429f3175d38
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-05 16:33:23 -0500

    Solve dominoes problem

--- kotlin/dominoes/src/main/kotlin/Dominoes.kt
+++ kotlin/dominoes/src/main/kotlin/Dominoes.kt
@@ -5,62 +5,42 @@ data class Domino(val left: Int, val right: Int)
 object Dominoes {
     fun formChain(inputDominoes: List<Domino>): List<Domino> {
         fun tryFormChain(
-            indexes: MutableSet<Int>,
-            chain: MutableList<Domino>
+            dominoes: List<Domino>,
+            accumulator: List<Domino>
         ): List<Domino> {
-            if (chain.size == inputDominoes.size) {
-                return chain
-            }
-            for ((index, domino) in inputDominoes.withIndex()) {
-                if (index in indexes) {
-                    continue
+            if (dominoes.isEmpty()) {
+                if (accumulator.size != inputDominoes.size
+                    || !accumulator.last().isChain(accumulator.first())) {
+                    return listOf()
                 }
-                indexes += index
-                val last = chain.lastOrNull()
-                if (last == null) {
-                    chain += domino
-                    val c = tryFormChain(indexes, chain)
-                    if (c.isNotEmpty()) {
-                        return c
-                    }
+                return accumulator
+            }
+            return dominoes.flatMapIndexed { index, domino ->
+                val remaining =
+                    dominoes.subList(0, index) + dominoes.subList(index + 1, dominoes.size)
+                val chain = if (accumulator.isEmpty()) {
+                    tryFormChain(remaining, accumulator + domino)
+                } else if (accumulator.last().isChain(domino)) {
+                    tryFormChain(remaining, accumulator + domino)
+                } else if (accumulator.last().isChain(domino.flip())) {
+                    tryFormChain(remaining, accumulator + domino.flip())
                 } else {
-                    when (last.right) {
-                        domino.left -> {
-                            if (index != inputDominoes.lastIndex
-                                || (index == inputDominoes.lastIndex && domino.isMatch(chain.first()))) {
-                                chain += domino
-                                val c = tryFormChain(indexes, chain)
-                                if (c.isNotEmpty()) {
-                                    return c
-                                }
-                            }
-                        }
-
-                        domino.right -> {
-                            if (index != inputDominoes.lastIndex
-                                || (index == inputDominoes.lastIndex && domino.isMatch(chain.first()))) {
-                                chain += domino.flip()
-                                val c = tryFormChain(indexes, chain)
-                                if (c.isNotEmpty()) {
-                                    return c
-                                }
-                            }
-                        }
-                    }
+                    tryFormChain(remaining, accumulator)
                 }
-                indexes -= index
+                if (chain.isNotEmpty()) {
+                    // Short circuit.
+                    return chain
+                } else {
+                    chain
+                }
             }
-            if (chain.isNotEmpty()) {
-                chain.removeLast()
-            }
-            return listOf()
         }
 
         if (inputDominoes.isEmpty()) {
             return listOf()
         }
-        return tryFormChain(mutableSetOf(), mutableListOf()).also {
-            if (it.isEmpty() || !it.last().isMatch(it.first())) {
+        return tryFormChain(inputDominoes, listOf()).also {
+            if (it.isEmpty()) {
                 throw ChainNotFoundException("Could not form a chain")
             }
         }
@@ -72,5 +52,5 @@ object Dominoes {
 
     private fun Domino.flip() = Domino(this.right, this.left)
 
-    private fun Domino.isMatch(domino: Domino) = this.right == domino.left
+    private fun Domino.isChain(domino: Domino) = this.right == domino.left
 }
