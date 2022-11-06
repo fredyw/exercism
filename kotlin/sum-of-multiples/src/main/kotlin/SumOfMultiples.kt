cef2753928be415d122f5cda405c49c1d32a8b17
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-06 11:41:21 -0600

    Solve sum-of-multiplies problem

--- kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,6 +1,7 @@
 object SumOfMultiples {
-
     fun sum(factors: Set<Int>, limit: Int): Int {
-        TODO("Implement this function to complete the task")
+        return (1 until limit).sumOf { number ->
+            if (factors.filter { it != 0 }.any { number % it == 0 }) number else 0
+        }
     }
 }
