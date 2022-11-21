ab22abf1a3f594b4d983c67040260b8c146d6873
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-21 15:21:23 -0600

    Solve kindergarten-garden problem

--- kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -1,5 +1,26 @@
 class KindergartenGarden(private val diagram: String) {
+    private val students: List<String> = listOf(
+        "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet",
+        "Ileana", "Joseph", "Kincaid", "Larry"
+    )
+
+    private val plantToStudents: Map<String, List<String>> = run {
+        val (row1, row2) = diagram.split("\n")
+        val plants = row1.chunked(2).zip(row2.chunked(2))
+            .map { (a, b) -> (a + b).map { it.toPlant() } }
+        students.zip(plants).toMap()
+    }
+
     fun getPlantsOfStudent(student: String): List<String> {
-        TODO("Implement the function to complete the task")
+        return checkNotNull(plantToStudents[student])
+    }
+
+    private fun Char.toPlant(): String {
+        return when (this) {
+            'G' -> "grass"
+            'C' -> "clover"
+            'R' -> "radishes"
+            else -> "violets"
+        }
     }
 }
