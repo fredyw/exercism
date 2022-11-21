6e5431ddc482fd1ddec1b93df8d16be713b070df
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-20 20:05:21 -0600

    Solve grains problem

--- kotlin/grains/src/main/kotlin/Grains.kt
+++ kotlin/grains/src/main/kotlin/Grains.kt
@@ -1,12 +1,19 @@
 import java.math.BigInteger
+import kotlin.math.pow
 
 object Board {
+    private val grainCounts: Map<Int, BigInteger> =
+        (2..64).scan(1 to BigInteger.valueOf(1)) { acc, i ->
+            i to acc.second.multiply(BigInteger.valueOf(2))
+        }.toMap()
+
+    private val totalGrainCounts: BigInteger = grainCounts.values.sumOf { it }
 
     fun getGrainCountForSquare(number: Int): BigInteger {
-        TODO("Implement this function to complete the task")
+        return grainCounts[number] ?: throw IllegalArgumentException()
     }
 
     fun getTotalGrainCount(): BigInteger {
-        TODO("Implement this function to complete the task")
+        return totalGrainCounts
     }
 }
