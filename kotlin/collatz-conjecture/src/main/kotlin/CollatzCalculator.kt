e2973449c708b99bc10915a09884c704ff1e7241
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-21 13:12:06 -0600

    Solve collatz-conjecture problem

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,5 +1,12 @@
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+        require(start > 0)
+        return generateSequence(start) {
+            if (it % 2 == 0) {
+                it / 2
+            } else {
+                (3 * it) + 1
+            }
+        }.takeWhile { it > 1 }.count()
     }
 }
