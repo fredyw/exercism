2da2b6ed375249a4796f4480099070e45e045c10
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-21 16:13:17 -0600

    Solve nth-prime problem

--- kotlin/nth-prime/src/main/kotlin/NthPrime.kt
+++ kotlin/nth-prime/src/main/kotlin/NthPrime.kt
@@ -1,6 +1,23 @@
 object Prime {
-
     fun nth(n: Int): Int {
-        TODO("Implement this function to complete the task")
+        require(n > 0) { "There is no zeroth prime." }
+        return generateSequence(2) { it + 1 }
+            .filter { it.isPrime() }
+            .elementAt(n - 1)
+    }
+
+    private fun Int.isPrime(): Boolean {
+        if (this == 2 || this == 3) {
+            return true
+        }
+        if (this <= 1 || this % 2 == 0 || this % 3 == 0) {
+            return false
+        }
+        if (generateSequence(5) { it + 6 }
+            .takeWhile { it * it <= this }
+            .any { this % it == 0 || this % (it + 2) == 0 }) {
+            return false
+        }
+        return true
     }
 }
--- kotlin/nth-prime/src/test/kotlin/PrimeTest.kt
+++ kotlin/nth-prime/src/test/kotlin/PrimeTest.kt
@@ -1,11 +1,9 @@
 import org.junit.Test
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
 
 class PrimeTest {
-
     @Rule
     @JvmField
     var expectedException: ExpectedException = ExpectedException.none()
@@ -15,25 +13,21 @@ class PrimeTest {
         assertEquals(2, Prime.nth(1))
     }
 
-    @Ignore
     @Test
     fun secondPrime() {
         assertEquals(3, Prime.nth(2))
     }
 
-    @Ignore
     @Test
     fun sixthPrime() {
         assertEquals(13, Prime.nth(6))
     }
 
-    @Ignore
     @Test
     fun bigPrime() {
         assertEquals(104743, Prime.nth(10001))
     }
 
-    @Ignore
     @Test
     fun undefinedPrime() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -41,5 +35,4 @@ class PrimeTest {
 
         Prime.nth(0)
     }
-
 }
