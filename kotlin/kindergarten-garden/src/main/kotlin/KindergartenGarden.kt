623803f290279a42ff39e25708773d650c97a04c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-21 13:30:15 -0600

    Add kindergarten-garden problem

---
+++ kotlin/kindergarten-garden/src/main/kotlin/KindergartenGarden.kt
@@ -0,0 +1,5 @@
+class KindergartenGarden(private val diagram: String) {
+    fun getPlantsOfStudent(student: String): List<String> {
+        TODO("Implement the function to complete the task")
+    }
+}
