2ece3ae694bf380623f03182606fc363e1137c59
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-21 20:22:39 -0600

    Solve pascals-triangle problem

--- kotlin/pascals-triangle/src/main/kotlin/PascalsTriangle.kt
+++ kotlin/pascals-triangle/src/main/kotlin/PascalsTriangle.kt
@@ -1,6 +1,13 @@
 object PascalsTriangle {
-
     fun computeTriangle(rows: Int): List<List<Int>> {
-        TODO("Implement this function to complete the task")
+        return if (rows == 0) {
+            listOf()
+        } else {
+            (0 until rows - 1).scan(listOf(1)) { acc, _ ->
+                listOf(acc.first()) +
+                        (0 until acc.size - 1).map { acc[it] + acc[it + 1] } +
+                        listOf(acc.last())
+            }
+        }
     }
 }
