cf19e84d570aa62370a6c7a9940853b16fc88331
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-22 00:32:01 -0600

    Solve prime-factors problem

--- kotlin/prime-factors/src/main/kotlin/PrimeFactors.kt
+++ kotlin/prime-factors/src/main/kotlin/PrimeFactors.kt
@@ -1,10 +1,20 @@
 object PrimeFactorCalculator {
-
     fun primeFactors(int: Int): List<Int> {
-        TODO("Implement this function to complete the task")
+        return primeFactors(int.toLong()).map { it.toInt() }
     }
 
     fun primeFactors(long: Long): List<Long> {
-        TODO("Implement this function to complete the task")
+        return primeFactors(long, 2, listOf())
+    }
+
+    private tailrec fun primeFactors(number: Long, divisor: Long, acc: List<Long>): List<Long> {
+        if (number <= 1) {
+            return acc
+        }
+        return if (number % divisor == 0L) {
+            primeFactors(number / divisor, divisor, acc + divisor)
+        } else {
+            primeFactors(number, divisor + 1, acc)
+        }
     }
 }
