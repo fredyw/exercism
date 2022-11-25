2a6f29d163b08d1ade5a51a9b39a5ee23d7dbb26
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-25 00:11:59 -0600

    Solve darts problem

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,13 @@
-object Darts {
+import kotlin.math.hypot
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+object Darts {
+    fun score(x: Number, y: Number): Int {
+        val radius = hypot(x.toDouble(), y.toDouble())
+        return when {
+            radius <= 1 -> 10
+            radius <= 5 -> 5
+            radius <= 10 -> 1
+            else -> 0
+        }
     }
 }
