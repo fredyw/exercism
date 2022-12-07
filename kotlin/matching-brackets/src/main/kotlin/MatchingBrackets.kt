c33da5ec9e9683d981b415b233b2784632c8a220
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-07 00:11:31 -0600

    Solve matching-brackets problem

--- kotlin/matching-brackets/src/main/kotlin/MatchingBrackets.kt
+++ kotlin/matching-brackets/src/main/kotlin/MatchingBrackets.kt
@@ -1,6 +1,22 @@
 object MatchingBrackets {
+    private val OPENING_BRACKETS = setOf('(', '{', '[')
+    private val CLOSING_BRACKETS = setOf(')', '}', ']')
+    private val BRACKETS = OPENING_BRACKETS + CLOSING_BRACKETS
 
     fun isValid(input: String): Boolean {
-        TODO("Implement this function to complete the task")
+        val deque = ArrayDeque<Char>()
+        return input.filter { it in BRACKETS }.all {
+            if (it in OPENING_BRACKETS) {
+                deque.addFirst(it)
+                return true
+            } else {
+                return when (deque.removeLastOrNull()) {
+                    '(' -> it == ')'
+                    '{' -> it == '}'
+                    '[' -> it == ']'
+                    else -> false
+                }
+            }
+        }
     }
 }
--- kotlin/matching-brackets/src/test/kotlin/MatchingBracketsTest.kt
+++ kotlin/matching-brackets/src/test/kotlin/MatchingBracketsTest.kt
@@ -1,6 +1,4 @@
-
 import org.junit.Test
-import org.junit.Ignore
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import kotlin.test.assertEquals
