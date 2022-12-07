ca0702893055b8d4956bbc95f23c2adf9152891c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-06 23:47:14 -0600

    Solve dnd-character problem

--- kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
+++ kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
@@ -1,22 +1,21 @@
-class DndCharacter {
+import kotlin.math.floor
 
-    val strength: Int = TODO("Initialize value to complete the task")
-    val dexterity: Int = TODO("Initialize value to complete the task")
-    val constitution: Int = TODO("Initialize value to complete the task")
-    val intelligence: Int = TODO("Initialize value to complete the task")
-    val wisdom: Int = TODO("Initialize value to complete the task")
-    val charisma: Int = TODO("Initialize value to complete the task")
-    val hitpoints: Int = TODO("Initialize value to complete the task")
+class DndCharacter {
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints: Int = 10 + modifier(constitution)
 
     companion object {
-
         fun ability(): Int {
-            TODO("Implement the function to complete the task")
+            return (1..4).map { (1..6).random() }.sortedDescending().take(3).sum()
         }
 
         fun modifier(score: Int): Int {
-            TODO("Implement the function to complete the task")
+            return floor((score - 10) / 2.0).toInt()
         }
     }
-
 }
