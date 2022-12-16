769a43182f50ee1ec6c82a70e408c78831e29253
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-16 00:31:01 -0600

    Solve complex-numbers problem

--- kotlin/complex-numbers/src/main/kotlin/ComplexNumber.kt
+++ kotlin/complex-numbers/src/main/kotlin/ComplexNumber.kt
@@ -1,17 +1,40 @@
+import kotlin.math.*
+
 data class ComplexNumber(val real: Double = 0.0, val imag: Double = 0.0) {
-    val abs: Double = TODO()
+    val abs: Double = sqrt(real.pow(2) + imag.pow(2))
 }
 
-operator fun ComplexNumber.plus(complexNumber: ComplexNumber): ComplexNumber = TODO()
+operator fun ComplexNumber.plus(other: ComplexNumber): ComplexNumber =
+    ComplexNumber(
+        real = this.real + other.real,
+        imag = this.imag + other.imag
+    )
 
-operator fun ComplexNumber.minus(complexNumber: ComplexNumber): ComplexNumber = TODO()
+operator fun ComplexNumber.minus(other: ComplexNumber): ComplexNumber =
+    ComplexNumber(
+        real = this.real - other.real,
+        imag = this.imag - other.imag
+    )
 
-operator fun ComplexNumber.times(complexNumber: ComplexNumber): ComplexNumber = TODO()
+operator fun ComplexNumber.times(other: ComplexNumber): ComplexNumber =
+    ComplexNumber(
+        real = this.real * other.real - this.imag * other.imag,
+        imag = this.imag * other.real + this.real * other.imag
+    )
 
-operator fun ComplexNumber.div(complexNumber: ComplexNumber): ComplexNumber = TODO()
+operator fun ComplexNumber.div(other: ComplexNumber): ComplexNumber =
+    ComplexNumber(
+        real = (this.real * other.real + this.imag * other.imag) /
+                (other.real.pow(2) + other.imag.pow(2)),
+        imag = (this.imag * other.real - this.real * other.imag) /
+                (other.real.pow(2) + other.imag.pow(2))
+    )
 
-fun ComplexNumber.conjugate(): ComplexNumber = TODO()
+fun ComplexNumber.conjugate(): ComplexNumber =
+    ComplexNumber(real = this.real, imag = -this.imag)
 
-fun exponential(complexNumber: ComplexNumber): ComplexNumber {
-    TODO()
-}
+fun exponential(c: ComplexNumber): ComplexNumber =
+    ComplexNumber(
+        real = exp(c.real) * cos(c.imag),
+        imag = exp(c.real) * sin(c.imag)
+    )
