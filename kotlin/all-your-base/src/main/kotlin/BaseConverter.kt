fd8dc869a68da39f68468749a10125ac7a0a223c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-27 00:14:22 -0600

    Solve all-your-base problem

--- kotlin/all-your-base/src/main/kotlin/BaseConverter.kt
+++ kotlin/all-your-base/src/main/kotlin/BaseConverter.kt
@@ -1,7 +1,29 @@
-class BaseConverter {
-    // TODO: implement proper constructor to complete the task
+import kotlin.math.pow
+
+class BaseConverter(private val base: Int, private val digits: IntArray) {
+    init {
+        require(base >= 2) { "Bases must be at least 2." }
+        require(digits.isNotEmpty()) { "You must supply at least one digit." }
+        if (digits.size > 1) {
+            require(digits.first() != 0) { "Digits may not contain leading zeros." }
+        }
+        require(digits.all { it >= 0 }) { "Digits may not be negative." }
+        require(digits.all { it < base }) { "All digits must be strictly less than the base." }
+    }
+
+    private val digits10: Int = digits.fold(0) { acc, v -> acc * base + v }
 
     fun convertToBase(newBase: Int): IntArray {
-        TODO("Implement the function to complete the task")
+        require(newBase >= 2) { "Bases must be at least 2." }
+        if (digits10 == 0) {
+            return intArrayOf(0)
+        }
+        fun convert(num: Int): IntArray {
+            if (num == 0) {
+                return intArrayOf()
+            }
+            return convert(num / newBase) + (num % newBase)
+        }
+        return convert(digits10)
     }
 }
--- kotlin/all-your-base/src/test/kotlin/BaseConverterTest.kt
+++ kotlin/all-your-base/src/test/kotlin/BaseConverterTest.kt
@@ -1,12 +1,10 @@
 import org.junit.Assert.assertArrayEquals
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
 import java.util.*
 
 class BaseConverterTest {
-
     /*
      * See https://github.com/junit-team/junit4/wiki/Rules for information on JUnit Rules in general and
      * ExpectedExceptions in particular.
@@ -27,7 +25,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testBinaryToSingleDecimal() {
         val baseConverter = BaseConverter(2, intArrayOf(1, 0, 1))
@@ -41,7 +38,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testSingleDecimalToBinary() {
         val baseConverter = BaseConverter(10, intArrayOf(5))
@@ -55,7 +51,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testBinaryToMultipleDecimal() {
         val baseConverter = BaseConverter(2, intArrayOf(1, 0, 1, 0, 1, 0))
@@ -69,7 +64,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testDecimalToBinary() {
         val baseConverter = BaseConverter(10, intArrayOf(4, 2))
@@ -83,7 +77,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testTrinaryToHexadecimal() {
         val baseConverter = BaseConverter(3, intArrayOf(1, 1, 2, 0))
@@ -97,7 +90,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testHexadecimalToTrinary() {
         val baseConverter = BaseConverter(16, intArrayOf(2, 10))
@@ -111,7 +103,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun test15BitInteger() {
         val baseConverter = BaseConverter(97, intArrayOf(3, 46, 60))
@@ -125,7 +116,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testEmptyDigits() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -134,7 +124,6 @@ class BaseConverterTest {
         BaseConverter(2, intArrayOf())
     }
 
-    @Ignore
     @Test
     fun testSingleZero() {
         val baseConverter = BaseConverter(10, intArrayOf(0))
@@ -148,7 +137,6 @@ class BaseConverterTest {
             actualDigits)
     }
 
-    @Ignore
     @Test
     fun testMultipleZeros() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -157,7 +145,6 @@ class BaseConverterTest {
         BaseConverter(10, intArrayOf(0, 0, 0))
     }
 
-    @Ignore
     @Test
     fun testLeadingZeros() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -166,7 +153,6 @@ class BaseConverterTest {
         BaseConverter(7, intArrayOf(0, 6, 0))
     }
 
-    @Ignore
     @Test
     fun testFirstBaseIsOne() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -175,7 +161,6 @@ class BaseConverterTest {
         BaseConverter(1, intArrayOf(0))
     }
 
-    @Ignore
     @Test
     fun testFirstBaseIsZero() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -184,7 +169,6 @@ class BaseConverterTest {
         BaseConverter(0, intArrayOf())
     }
 
-    @Ignore
     @Test
     fun testFirstBaseIsNegative() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -193,7 +177,6 @@ class BaseConverterTest {
         BaseConverter(-2, intArrayOf(1))
     }
 
-    @Ignore
     @Test
     fun testNegativeDigit() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -202,7 +185,6 @@ class BaseConverterTest {
         BaseConverter(2, intArrayOf(1, -1, 1, 0, 1, 0))
     }
 
-    @Ignore
     @Test
     fun testInvalidPositiveDigit() {
         expectedException.expect(IllegalArgumentException::class.java)
@@ -211,7 +193,6 @@ class BaseConverterTest {
         BaseConverter(2, intArrayOf(1, 2, 1, 0, 1, 0))
     }
 
-    @Ignore
     @Test
     fun testSecondBaseIsOne() {
         val baseConverter = BaseConverter(2, intArrayOf(1, 0, 1, 0, 1, 0))
@@ -222,7 +203,6 @@ class BaseConverterTest {
         baseConverter.convertToBase(1)
     }
 
-    @Ignore
     @Test
     fun testSecondBaseIsZero() {
         val baseConverter = BaseConverter(2, intArrayOf(1, 0, 1, 0, 1, 0))
@@ -233,7 +213,6 @@ class BaseConverterTest {
         baseConverter.convertToBase(0)
     }
 
-    @Ignore
     @Test
     fun testSecondBaseIsNegative() {
         val baseConverter = BaseConverter(2, intArrayOf(1))
