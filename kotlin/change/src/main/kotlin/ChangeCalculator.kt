ac08f02d94789fa4eb7695088c92e980252af546
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-27 14:40:34 -0600

    Solve change problem

--- kotlin/change/src/main/kotlin/ChangeCalculator.kt
+++ kotlin/change/src/main/kotlin/ChangeCalculator.kt
@@ -1,7 +1,33 @@
-class ChangeCalculator {
-    // TODO: implement proper constructor
-
+class ChangeCalculator(private val denominations: List<Int>) {
     fun computeMostEfficientChange(grandTotal: Int): List<Int> {
-        TODO("Implement the function to complete the task")
+        require(grandTotal >= 0) { "Negative totals are not allowed." }
+        fun compute(total: Int, memo: MutableMap<Int, List<Int>>): List<Int>? {
+            if (total == 0) {
+                return listOf()
+            }
+            if (total < 0) {
+                return null
+            }
+            val c = memo[total]
+            if (c != null) {
+                return c
+            }
+            val minChange = denominations.mapNotNull {
+                val change = compute(total - it, memo)
+                if (change != null) {
+                    change + listOf(it)
+                } else {
+                    null
+                }
+            }.minOfWithOrNull(compareBy { it.size }) { it }
+            if (minChange != null) {
+                memo[total] = minChange
+            }
+            return minChange
+        }
+
+        val change = compute(grandTotal, mutableMapOf())
+        return change
+            ?: throw IllegalArgumentException("The total $grandTotal cannot be represented in the given currency.")
     }
 }
