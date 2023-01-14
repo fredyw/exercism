bae60067e049ab415c39deaebc5d89e6f6be66d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-14 09:39:55 -0600

    Solve grade-school problem

--- kotlin/grade-school/src/main/kotlin/GradeSchool.kt
+++ kotlin/grade-school/src/main/kotlin/GradeSchool.kt
@@ -1,14 +1,24 @@
+import java.util.TreeMap
+import java.util.TreeSet
+
 class School {
+    private val map = TreeMap<Int, TreeSet<String>>()
 
     fun add(student: String, grade: Int) {
-        TODO("Implement this function to complete the task")
+        val students = map[grade]
+        if (students == null) {
+            map[grade] = TreeSet<String>().also { it += student }
+        } else {
+            students.add(student)
+        }
     }
 
     fun grade(grade: Int): List<String> {
-        TODO("Implement this function to complete the task")
+        val students = map[grade] ?: return listOf()
+        return students.toList()
     }
 
     fun roster(): List<String> {
-        TODO("Implement this function to complete the task")
+        return map.flatMap { it.value.toList() }
     }
 }
