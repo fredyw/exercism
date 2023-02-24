2cf96a4aeb4f3e75775de465e423520d883bc254
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-24 16:33:26 -0600

    Solve simpler-cipher problem

--- kotlin/simple-cipher/src/main/kotlin/SimpleCipher.kt
+++ kotlin/simple-cipher/src/main/kotlin/SimpleCipher.kt
@@ -1,12 +1,37 @@
-data class Cipher(val todo: Nothing) {
+import kotlin.random.Random
 
-    // TODO: Implement proper constructor
+data class Cipher(val key: String = generateRandom()) {
+    init {
+        require(key.isNotEmpty())
+        require(key.none { it.isDigit() || it.isUpperCase() })
+    }
+
+    private val shifts = key.map { it - 'a' }
 
     fun encode(s: String): String {
-        TODO("Implement this function to complete the task")
+        return s.toCharArray().mapIndexed { index, char ->
+            val shift = shifts[index % shifts.size]
+            val i = (char - 'a' + shift) % ALPHABET.length
+            ALPHABET[i]
+        }.joinToString("")
     }
 
     fun decode(s: String): String {
-        TODO("Implement this function to complete the task")
+        return s.toCharArray().mapIndexed { index, char ->
+            val shift = shifts[index % shifts.size]
+            val i =
+                ((char - 'a' - shift) % ALPHABET.length).let { if (it < 0) it + ALPHABET.length else it }
+            ALPHABET[i]
+        }.joinToString("")
+    }
+
+    companion object {
+        private val ALPHABET = ('a'..'z').joinToString("")
+
+        private fun generateRandom(): String {
+            val index = Random.nextInt(ALPHABET.length + 1)
+            return (0 until 100).map { ALPHABET[index] }.joinToString("")
+        }
     }
 }
+
--- kotlin/simple-cipher/src/test/kotlin/RandomKeyCipherTest.kt
+++ kotlin/simple-cipher/src/test/kotlin/RandomKeyCipherTest.kt
@@ -1,29 +1,24 @@
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 class RandomKeyCipherTest {
-
     @Test
     fun `can encode`() = with(Cipher()) {
         assertEquals(key.substring(0, 10), encode("aaaaaaaaaa"))
     }
 
-    @Ignore
     @Test
     fun `can decode`() = with(Cipher()) {
         assertEquals("aaaaaaaaaa", decode(key.substring(0, 10)))
     }
 
-    @Ignore
     @Test
     fun `key is made only of lowercase letters`() {
         assertTrue(Cipher().key.matches(Regex("[a-z]+")))
     }
 
-    @Ignore
     @Test
     fun `is reversible`() {
         val plainText = "abcdefghij"
@@ -32,16 +27,13 @@ class RandomKeyCipherTest {
 
     // extra tests
 
-    @Ignore
     @Test
     fun `default key is of length 100`() {
         assertEquals(100, Cipher().key.length)
     }
 
-    @Ignore
     @Test
     fun `two default generated keys differs`() {
         assertNotEquals(Cipher().key, Cipher().key)
     }
-
 }
