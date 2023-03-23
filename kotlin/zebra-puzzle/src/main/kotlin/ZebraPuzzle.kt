8bd661c0027ba012d98f4b0026e89a5c6fe6bf93
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-22 19:20:14 -0500

    Update zebra-puzzle problem

--- kotlin/zebra-puzzle/src/main/kotlin/ZebraPuzzle.kt
+++ kotlin/zebra-puzzle/src/main/kotlin/ZebraPuzzle.kt
@@ -1,4 +1,23 @@
 class ZebraPuzzle() {
+    enum class Color {
+        YELLOW, BLUE, RED, IVORY, GREEN
+    }
+
+    enum class Nationality {
+        NORWEGIAN, UKRAINIAN, ENGLISHMAN, SPANIARD, JAPANESE
+    }
+
+    enum class Drink {
+        WATER, TEA, MILK, ORANGE_JUICE, COFFEE
+    }
+
+    enum class Smoke {
+        KOOLS, CHESTERFIELD, OLD_GOLD, LUCKY_STRIKE, PARLIAMENT
+    }
+
+    enum class Pet {
+        FOX, HORSE, SNAILS, DOG, ZEBRA
+    }
 
     fun drinksWater(): String {
         TODO("Implement this function to complete the task")
@@ -8,4 +27,13 @@ class ZebraPuzzle() {
         TODO("Implement this function to complete the task")
     }
 
+    private inline fun <reified T : Enum<T>> permutations(): List<List<T>> =
+        enumValues<T>().toList().permute()
+
+    private fun <T> List<T>.permute(): List<List<T>> {
+        if (isEmpty()) {
+            return listOf(this)
+        }
+        return flatMap { e -> (this - e).permute().map { list -> list + e } }
+    }
 }
--- kotlin/zebra-puzzle/src/test/kotlin/ZebraPuzzleTest.kt
+++ kotlin/zebra-puzzle/src/test/kotlin/ZebraPuzzleTest.kt
@@ -1,13 +1,10 @@
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class ZebraPuzzleTest {
-
     @Test
     fun `resident who drinks water`() = assertEquals("Norwegian", ZebraPuzzle().drinksWater())
 
     @Test
     fun `resident who owns zebra`() = assertEquals("Japanese", ZebraPuzzle().ownsZebra())
-
 }
