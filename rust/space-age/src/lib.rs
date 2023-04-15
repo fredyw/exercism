dd1c2702b1f8bcac5a0f03da83dd7ab75f6a45d5
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-15 15:07:17 -0500

    Solve spage-age problem

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -2,36 +2,36 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {s}")
+        Self(s as f64 / 31_557_600 as f64)
     }
 }
-
 pub trait Planet {
+    const PERIOD: f64;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({d:?}) to the number of years on this planet for that duration"
-        );
+        d.0 / Self::PERIOD
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($s:ident, $p:expr) => {
+        pub struct $s;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+        impl Planet for $s {
+            const PERIOD: f64 = $p;
+        }
+    };
+}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
