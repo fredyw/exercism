3180246f45c2bdeb561e03d0e959b7f16b1a6e4f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-20 01:05:29 -0500

    Solve sublist problem

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::Comparison::{Equal, Sublist, Superlist, Unequal};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -7,5 +9,17 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    fn is_sublist_of<T: PartialEq>(first: &[T], second: &[T]) -> bool {
+        first.is_empty() || second.windows(first.len()).any(|e| e == first)
+    }
+
+    if _first_list == _second_list {
+        Equal
+    } else if is_sublist_of(_first_list, _second_list) {
+        Sublist
+    } else if is_sublist_of(_second_list, _first_list) {
+        Superlist
+    } else {
+        Unequal
+    }
 }
