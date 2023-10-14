cd0c138bd3506e79ceb9452739099e760ba742a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-14 14:01:35 -0500

    Solve matching-brackets problem

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut vec: Vec<char> = vec![];
+    for char in string.chars() {
+        if char == '[' || char == '{' || char == '(' {
+            vec.push(char);
+        } else if char == ']' || char == '}' || char == ')' {
+            let front = vec.pop();
+            match front {
+                Some(c) => {
+                    if (c == '[' && char != ']')
+                        || (c == '{' && char != '}')
+                        || (c == '(' && char != ')')
+                    {
+                        return false;
+                    }
+                }
+                None => {
+                    return false;
+                }
+            }
+        }
+    }
+    vec.is_empty()
 }
