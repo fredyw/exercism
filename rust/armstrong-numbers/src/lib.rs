7f40390aa639426ec952a4e6be501346e0da056b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-14 14:28:35 -0500

    Solve armstrong-numbers problem

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let string = format!("{}", num);
+    let sum = string
+        .chars()
+        .into_iter()
+        .map(|c| (c.to_digit(10).unwrap() as u64).pow(string.len() as u32))
+        .sum::<u64>();
+    sum == num as u64
 }
