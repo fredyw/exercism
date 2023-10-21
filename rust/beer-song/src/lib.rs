96d722b3617a503a62775386455f3aec19ed9569
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-21 15:05:10 -0500

    Solve beer-song problem

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,34 @@
 pub fn verse(n: u32) -> String {
-    todo!("emit verse {n}")
+    fn n_bottles(n: u32) -> String {
+        if n == 0 {
+            "no more bottles".to_string()
+        } else if n == 1 {
+            format!("{} bottle", n)
+        } else {
+            format!("{} bottles", n)
+        }
+    }
+    if n > 0 {
+        format!(
+            "{} of beer on the wall, {} of beer.\n\
+            Take {} down and pass it around, {} of beer on the wall.\n",
+            n_bottles(n),
+            n_bottles(n),
+            if n == 1 { "it" } else { "one" },
+            n_bottles(n - 1),
+        )
+    } else {
+        format!(
+            "No more bottles of beer on the wall, no more bottles of beer.\n\
+            Go to the store and buy some more, 99 bottles of beer on the wall.\n",
+        )
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    todo!("sing verses {start} to {end}, inclusive")
+    (end..=start)
+        .rev()
+        .map(|n| verse(n))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
