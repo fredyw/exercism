3322cfaf7ec01a0fbdaf276e1d768b004541e837
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-04 01:15:33 -0500

    Solve bob problem

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn reply(message: &str) -> &str {
-    todo!("have Bob reply to the incoming message: {message}")
+    let has_alphabet = message.chars().any(|c| c.is_alphabetic());
+    match message.trim() {
+        m if has_alphabet && m.to_uppercase() == message && m.ends_with("?") => {
+            "Calm down, I know what I'm doing!"
+        }
+        m if has_alphabet && m.to_uppercase() == message => "Whoa, chill out!",
+        m if m.ends_with("?") => "Sure.",
+        m if m.is_empty() => "Fine. Be that way!",
+        _ => "Whatever.",
+    }
 }
