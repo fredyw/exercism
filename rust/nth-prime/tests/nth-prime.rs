7218b5a9a03a5663a92161b8f0250c27d9cc63ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-10 00:34:48 -0600

    Update nth-prime problem

--- rust/nth-prime/tests/nth-prime.rs
+++ rust/nth-prime/tests/nth-prime.rs
@@ -6,19 +6,16 @@ fn first_prime() {
 }
 
 #[test]
-#[ignore]
 fn second_prime() {
     assert_eq!(np::nth(1), 3);
 }
 
 #[test]
-#[ignore]
 fn sixth_prime() {
     assert_eq!(np::nth(5), 13);
 }
 
 #[test]
-#[ignore]
 fn big_prime() {
     assert_eq!(np::nth(10_000), 104_743);
 }
