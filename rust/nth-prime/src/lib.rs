5f55885fb6b6a4c1dfdb7544a514b228c938e915
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-10 16:44:21 -0600

    Update nth-prime problem

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    fn is_prime(n: u32) -> bool {
+        if n <= 1 {
+            return false;
+        }
+        (2..=(n as f32).sqrt().ceil() as u32)
+            .filter(|&num| num != n)
+            .all(|num| n % num != 0)
+    }
+
+    let mut i = 0;
+    let mut num = 2;
+    while i <= n {
+        while !is_prime(num) {
+            num += 1;
+        }
+        num += 1;
+        i += 1;
+    }
+    num - 1
 }
