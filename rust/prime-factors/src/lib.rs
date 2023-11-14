c950cee462a82d0f5a0434ba11de122c61e76a04
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-14 15:43:42 -0600

    Solve prime-factors problem

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut prime_factors = vec![];
+    let mut n = n;
+    while n > 1 {
+        for i in 2..=n {
+            if n % i == 0 {
+                prime_factors.push(i);
+                n /= i;
+                break;
+            }
+        }
+    }
+    prime_factors
 }
