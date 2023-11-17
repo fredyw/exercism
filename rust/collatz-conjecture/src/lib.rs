5c984f89d72d951eac2d88baebd860ef155ca235
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-17 14:46:48 -0600

    Solve collatz-conjecture problem

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,3 +1,15 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+    let mut steps = 0;
+    while n > 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = n.checked_mul(3)?.checked_add(1)?;
+        }
+        steps += 1;
+    }
+    Some(steps)
 }
