8114d8eb6144beb9a80594b162b91ebbc8146749
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-17 00:38:04 -0600

    Solve series problem

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
+    if len == 0 {
+        vec!["".to_string(); digits.len() + 1]
+    } else {
+        digits
+            .chars()
+            .collect::<Vec<char>>()
+            .windows(len)
+            .map(|w| w.iter().collect::<String>())
+            .collect::<Vec<String>>()
+    }
 }
