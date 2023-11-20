ddf9d3865e9aa8174f3dc31a7379aff3dd0915ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-20 11:41:04 -0600

    Solve diffie-hellman problem

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,11 +1,30 @@
+use rand::{thread_rng, Rng};
+
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+    thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    todo!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+    mod_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    todo!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
+    mod_pow(b_pub, a, p)
+}
+
+// https://en.wikipedia.org/wiki/Modular_exponentiation
+fn mod_pow(mut base: u64, mut exponent: u64, modulus: u64) -> u64 {
+    if modulus == 1 {
+        return 0;
+    }
+    let mut result = 1;
+    base = base % modulus;
+    while exponent > 0 {
+        if exponent % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exponent >>= 1;
+        base = (base * base) % modulus;
+    }
+    result
 }
