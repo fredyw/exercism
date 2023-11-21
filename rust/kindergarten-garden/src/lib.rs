3b065c9fc01fc78d9f734b0a5a69033b0fd3d377
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-21 12:01:40 -0600

    Solve kindergarten-garden problem

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,3 +1,24 @@
-pub fn plants(_diagram: &str, _student: &str) -> Vec<&'static str> {
-    todo!("Solve kindergarten-garden exercise");
+use std::collections::HashMap;
+
+const STUDENTS: [&str; 12] = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+    "Kincaid", "Larry",
+];
+
+pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
+    let plants = HashMap::from([
+        ('G', "grass"),
+        ('C', "clover"),
+        ('R', "radishes"),
+        ('V', "violets"),
+    ]);
+    let index = STUDENTS.iter().position(|&s| s == student).unwrap() * 2;
+    diagram
+        .lines()
+        .flat_map(|row| {
+            row[index..=index + 1]
+                .chars()
+                .map(|c| *plants.get(&c).unwrap())
+        })
+        .collect()
 }
