bebbb75cae475f8836432252ece178beeba81451
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-16 22:23:06 -0600

    Solve alphametics problem

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,53 +1,59 @@
 use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    fn str_to_num(s: &str, map: &HashMap<char, u8>) -> Option<u64> {
-        let mut string = String::new();
-        for char in s.chars() {
-            string.push_str(&map.get(&char).unwrap().to_string());
-            if string.starts_with("0") {
-                return None;
+    fn to_num(strings: &Vec<&str>, map: &HashMap<char, u8>) -> u64 {
+        let mut sum = 0;
+        for s in strings.iter() {
+            let mut num = 0;
+            for char in s.chars() {
+                let digit = *map.get(&char).unwrap() as u64;
+                num += digit;
+                num *= 10;
             }
+            sum += num;
         }
-        Some(string.parse::<u64>().unwrap())
+        sum
     }
 
     fn solve(
         lhs: &Vec<&str>,
         rhs: &str,
+        leading: &Vec<u8>,
         chars: &mut Vec<char>,
         nums: &mut [bool; 10],
         map: &mut HashMap<char, u8>,
     ) -> bool {
         if chars.is_empty() {
-            let lhs_num: Vec<Option<u64>> = lhs.iter().map(|s| str_to_num(*s, map)).collect();
-            let rhs_num = str_to_num(rhs, map);
-            if lhs_num.iter().any(|o| o.is_none()) || rhs_num.is_none() {
-                return false;
-            }
-            return lhs_num.into_iter().map(|o| o.unwrap()).sum::<u64>() == rhs_num.unwrap();
+            let lhs_num = to_num(lhs, map);
+            let rhs_num = to_num(&vec![rhs], map);
+            return lhs_num == rhs_num;
         }
-        let char = &chars.pop().unwrap();
+        let char = chars.pop().unwrap();
         for num in 0..10 {
-            if map.contains_key(char) || nums[num as usize] {
+            if nums[num as usize] || (num == 0 && leading.contains(&(char as u8))) {
                 continue;
             }
             nums[num as usize] = true;
-            map.insert(*char, num);
-            let found = solve(lhs, rhs, chars, nums, map);
+            map.insert(char, num);
+            let found = solve(lhs, rhs, leading, chars, nums, map);
             if found {
                 return true;
             }
-            map.remove(char);
             nums[num as usize] = false;
         }
-        chars.push(*char);
+        chars.push(char);
         false
     }
 
     let mut split = input.split(" == ");
     let lhs: Vec<&str> = split.next().unwrap().split(" + ").collect();
     let rhs = split.next().unwrap();
+    let leading: Vec<u8> = input
+        .split(|c| c == ' ' || c == '+' || c == '=')
+        .filter_map(|w| w.bytes().nth(0))
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
     let mut chars: Vec<char> = input
         .chars()
         .filter(|c| !" +=".contains(*c))
@@ -56,7 +62,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         .collect();
     let mut map: HashMap<char, u8> = HashMap::new();
     let mut nums = [false; 10];
-    if solve(&lhs, rhs, &mut chars, &mut nums, &mut map) {
+    if solve(&lhs, rhs, &leading, &mut chars, &mut nums, &mut map) {
         Some(map)
     } else {
         None
