1da6f422844fe7490e0d034ad4c24dd1759e1069
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-19 19:06:24 -0600

    Solve binary-search problem

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,15 +1,15 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+use std::cmp::Ordering;
+
+pub fn find<R: AsRef<[T]>, T: Ord>(collection: R, key: T) -> Option<usize> {
     let mut low: i32 = 0;
-    let mut high: i32 = array.len() as i32 - 1;
+    let collection = collection.as_ref();
+    let mut high: i32 = collection.len() as i32 - 1;
     while low <= high {
         let mid = low + ((high - low) / 2);
-        if key == array[mid as usize] {
-            return Some(mid as usize);
-        }
-        if array[mid as usize] < key {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+        match collection.as_ref()[mid as usize].cmp(&key) {
+            Ordering::Less => low = mid + 1,
+            Ordering::Equal => return Some(mid as usize),
+            Ordering::Greater => high = mid - 1,
         }
     }
     None
