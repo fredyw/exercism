f4e4db461572d6fc90a29802d5f8edc0aab5871e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-28 01:17:46 -0600

    Solve palindrome-products problem

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 /// `Palindrome` is a newtype which only exists when the contained value is a palindrome number in base ten.
 ///
 /// A struct with a single field which is used to constrain behavior like this is called a "newtype", and its use is
@@ -8,17 +10,42 @@ pub struct Palindrome(u64);
 impl Palindrome {
     /// Create a `Palindrome` only if `value` is in fact a palindrome when represented in base ten. Otherwise, `None`.
     pub fn new(value: u64) -> Option<Palindrome> {
-        todo!("if the value {value} is a palindrome return Some, otherwise return None");
+        let s = format!("{}", value);
+        let bytes = s.as_bytes();
+        let mut left = 0;
+        let mut right = bytes.len() - 1;
+        while left < right {
+            if bytes[left] != bytes[right] {
+                return None;
+            }
+            left += 1;
+            right -= 1;
+        }
+        Some(Palindrome { 0: value })
     }
 
     /// Get the value of this palindrome.
     pub fn into_inner(self) -> u64 {
-        todo!("return inner value of a Palindrome");
+        self.0
     }
 }
 
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    todo!(
-        "returns the minimum and maximum number of palindromes of the products of two factors in the range {min} to {max}"
-    );
+    let mut tree: BTreeSet<u64> = BTreeSet::new();
+    for i in min..=max {
+        for j in min..=max {
+            if let Some(p) = Palindrome::new(i * j) {
+                tree.insert(p.into_inner());
+            }
+        }
+    }
+
+    if tree.is_empty() {
+        None
+    } else {
+        Some((
+            Palindrome::new(*tree.iter().min().unwrap()).unwrap(),
+            Palindrome::new(*tree.iter().max().unwrap()).unwrap(),
+        ))
+    }
 }
