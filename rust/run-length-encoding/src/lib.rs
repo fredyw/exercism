502f5ee7db5b93f306b56c7fe6fa5c3f2e0264f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-30 00:41:16 -0600

    Update run-length-encoding problem

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,7 +1,33 @@
 pub fn encode(source: &str) -> String {
-    todo!("Return the run-length encoding of {source}.");
+    if source.is_empty() {
+        return "".to_string();
+    }
+    let mut encoded = String::new();
+    let chars: Vec<char> = source.chars().collect();
+    let mut i = 0;
+    let mut count = 1;
+    while i < chars.len() - 1 {
+        if chars[i] != chars[i + 1] {
+            if count > 1 {
+                encoded.push_str(&count.to_string());
+            }
+            encoded.push(chars[i]);
+            count = 1;
+        } else {
+            count += 1;
+        }
+        i += 1;
+    }
+    if count > 1 {
+        encoded.push_str(&count.to_string());
+    }
+    encoded.push(chars[i]);
+    encoded
 }
 
 pub fn decode(source: &str) -> String {
+    if source.is_empty() {
+        return "".to_string();
+    }
     todo!("Return the run-length decoding of {source}.");
 }
