4e9f99d3c527e13bd1094e77b9141b0a8d68c583
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-02 01:01:24 -0600

    Solve isbn-verifier problem

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -1,4 +1,22 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    todo!("Is {isbn:?} a valid ISBN number?");
+    let isbn = isbn.replace("-", "");
+    if isbn.len() != 10 {
+        return false;
+    }
+    match isbn
+        .chars()
+        .enumerate()
+        .try_fold(0, |acc, (i, c)| -> Result<u32, ()> {
+            if i == 9 && c == 'X' {
+                Ok(acc + ((10 - i as u32) * 10))
+            } else if c.is_numeric() {
+                Ok(acc + (10 - i as u32) * c.to_digit(10).unwrap())
+            } else {
+                Err(())
+            }
+        }) {
+        Ok(s) => s % 11 == 0,
+        Err(_) => false,
+    }
 }
