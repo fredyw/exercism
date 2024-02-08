9651e14c8db2e37711e72fdc9bcb30c337720849
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-08 15:29:15 -0600

    Solve triangle problem

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,19 +1,40 @@
-pub struct Triangle;
+use std::ops::Add;
 
-impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        todo!("Construct new Triangle from following sides: {sides:?}. Return None if the sides are invalid.");
+pub struct Triangle<T> {
+    a: T,
+    b: T,
+    c: T,
+}
+
+impl<T> Triangle<T>
+where
+    T: Copy + Default + PartialEq + PartialOrd + Add<Output = T>,
+{
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        let a = sides[0];
+        let b = sides[1];
+        let c = sides[2];
+        if a != T::default()
+            && b != T::default()
+            && c != T::default()
+            && a + b >= c
+            && b + c >= a
+            && a + c >= b
+        {
+            return Some(Triangle { a, b, c });
+        }
+        None
     }
 
     pub fn is_equilateral(&self) -> bool {
-        todo!("Determine if the Triangle is equilateral.");
+        self.a == self.b && self.b == self.c
     }
 
     pub fn is_scalene(&self) -> bool {
-        todo!("Determine if the Triangle is scalene.");
+        self.a != self.b && self.a != self.c && self.b != self.c
     }
 
     pub fn is_isosceles(&self) -> bool {
-        todo!("Determine if the Triangle is isosceles.");
+        self.a == self.b || self.a == self.c || self.b == self.c
     }
 }
