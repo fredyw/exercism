bfaef89c34ee67f74140320b790dc96f96121012
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-08 01:09:11 -0600

    Solve word-count problem

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -2,5 +2,18 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    todo!("Count of occurrences of words in {words:?}");
+    let mut map: HashMap<String, u32> = HashMap::new();
+    words
+        .to_lowercase()
+        .split(|c: char| c.is_whitespace() || c == ',')
+        .map(|word| {
+            word.trim_matches(|c: char| !c.is_ascii_alphanumeric())
+                .to_string()
+        })
+        .filter(|s| !s.is_empty())
+        .fold(&mut map, |acc, word| {
+            *acc.entry(word).or_insert(0) += 1;
+            acc
+        });
+    map
 }
