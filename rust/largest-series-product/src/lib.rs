4f2ba57b60427434570e06fc1f336e36c32c5c9c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-09 00:41:30 -0600

    Update largest-series-product problem

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::Error::{InvalidDigit, SpanTooLong};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     SpanTooLong,
@@ -5,5 +7,11 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    if string_digits.len() < span {
+        return Err(SpanTooLong);
+    }
+    if let Some(c) = string_digits.chars().find(|c| !c.is_numeric()) {
+        return Err(InvalidDigit(c));
+    }
     todo!("largest series product of a span of {span} digits in {string_digits}");
 }
