00ff96bfcb4bf169148b5b1e2f0916353acebc79
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-12 11:41:07 -0600

    Solve phone-number problem

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,16 +1,26 @@
 pub fn number(user_number: &str) -> Option<String> {
     let phone_number = user_number.replace(|c: char| !c.is_numeric(), "");
-    if phone_number.starts_with("0") {
-        None
-    } else if phone_number.len() == 11 {
-        Some(
-            phone_number.chars().collect::<Vec<char>>()[1..]
-                .iter()
-                .collect::<String>(),
-        )
+    if phone_number.len() == 11 {
+        let country_code = &phone_number[0..1];
+        let phone_number = &phone_number[1..];
+        if country_code != "1" {
+            None
+        } else {
+            get_number(phone_number)
+        }
     } else if phone_number.len() == 10 {
-        Some(phone_number)
+        get_number(&phone_number)
     } else {
         None
+    }
+}
+
+fn get_number(phone_number: &str) -> Option<String> {
+    let area_code = &phone_number[0..3];
+    let exchange_code = &phone_number[3..];
+    if area_code.starts_with(['0', '1']) || exchange_code.starts_with(['0', '1']) {
+        None
+    } else {
+        Some(phone_number.to_string())
     }
 }
