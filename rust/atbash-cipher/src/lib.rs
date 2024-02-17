b9f97ae66f7f980d824d579fdef3253beae54f40
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-17 16:02:57 -0600

    Solve atbash-cipher problem

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,33 @@
+use std::collections::HashMap;
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    todo!("Encoding of {plain:?} in Atbash cipher.");
+    let map: HashMap<char, char> = "abcdefghijklmnopqrstuvwxyz"
+        .chars()
+        .zip("zyxwvutsrqponmlkjihgfedcba".chars())
+        .collect();
+    plain
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| *map.get(&c).unwrap_or(&c))
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .map(|chunk| chunk.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    todo!("Decoding of {cipher:?} in Atbash cipher.");
+    let map: HashMap<char, char> = "zyxwvutsrqponmlkjihgfedcba"
+        .chars()
+        .zip("abcdefghijklmnopqrstuvwxyz".chars())
+        .collect();
+    cipher
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|c| *map.get(&c).unwrap_or(&c))
+        .collect()
 }
