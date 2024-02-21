9e9b9e07289ef9270ab4797d04be5200063dd0da
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-21 01:16:13 -0600

    Solve diamond problem

--- rust/diamond/src/lib.rs
+++ rust/diamond/src/lib.rs
@@ -1,25 +1,16 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    let length = (((c as u8 - 'A' as u8) * 2) + 1) as usize;
+    let length = ((c as usize - 'A' as usize) * 2) + 1;
     let mut diamond: Vec<Vec<char>> = vec![vec![' '; length]; length];
     let mid_row = (length - 1) / 2;
-    // Up
-    let mut col = 1;
-    let mut char = (c as u8 - 1) as char;
-    for row in (0..mid_row).rev() {
-        diamond[row][col] = char;
-        diamond[row][length - col - 1] = char;
-        col += 1;
-        char = (char as u8 - 1) as char;
-    }
-    // Center
     diamond[mid_row][0] = c;
     diamond[mid_row][length - 1] = c;
-    // Down
     let mut col = 1;
     let mut char = (c as u8 - 1) as char;
-    for row in mid_row + 1..length {
-        diamond[row][col] = char;
-        diamond[row][length - col - 1] = char;
+    for row in 0..mid_row {
+        diamond[mid_row - row - 1][col] = char;
+        diamond[mid_row - row - 1][length - col - 1] = char;
+        diamond[mid_row + row + 1][col] = char;
+        diamond[mid_row + row + 1][length - col - 1] = char;
         col += 1;
         char = (char as u8 - 1) as char;
     }
