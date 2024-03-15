08554f2a2bd658f6746255f3c0dd37a41cc0baf5
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-14 22:05:54 -0500

    Solve hamming problem

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,14 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    todo!("What is the Hamming Distance between {s1} and {s2}");
+    if s1.len() != s2.len() {
+        None
+    } else {
+        Some(
+            s1.chars()
+                .zip(s2.chars())
+                .map(|(a, b)| if a != b { 1 } else { 0 })
+                .sum(),
+        )
+    }
 }
