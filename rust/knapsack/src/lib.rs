947bf4e73f3eae5671b6153f902a4d58c28eddbb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-16 15:10:56 -0500

    Solve knapsack problem

--- rust/knapsack/src/lib.rs
+++ rust/knapsack/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct Item {
     pub weight: u32,
@@ -5,5 +7,30 @@ pub struct Item {
 }
 
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
+    fn maximum_value(
+        items: &[Item],
+        weight: i32,
+        index: usize,
+        memo: &mut HashMap<(usize, i32), u32>,
+    ) -> u32 {
+        if items.len() == index {
+            return 0;
+        }
+        if let Some(value) = memo.get(&(index, weight)) {
+            return *value;
+        }
+        let take = if weight - items[index].weight as i32 >= 0 {
+            maximum_value(items, weight - items[index].weight as i32, index + 1, memo)
+                + items[index].value
+        } else {
+            0
+        };
+        let skip = maximum_value(items, weight, index + 1, memo);
+        let max = take.max(skip);
+        memo.insert((index, weight), max);
+        max
+    }
+
+    let mut memo = HashMap::new();
+    maximum_value(items, max_weight as i32, 0, &mut memo)
 }
