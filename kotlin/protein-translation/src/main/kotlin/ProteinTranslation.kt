18909a16347bb6c6a245836561d78f5e361050b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 14:50:57 -0500

    Solve protein-translation problem

--- kotlin/protein-translation/src/main/kotlin/ProteinTranslation.kt
+++ kotlin/protein-translation/src/main/kotlin/ProteinTranslation.kt
@@ -1,3 +1,23 @@
 fun translate(rna: String?): List<String> {
-    TODO("Implement this function to complete the task")
+    if (rna == null) {
+        return listOf()
+    }
+    return rna.windowed(size = 3, step = 3, partialWindows = true)
+        .takeWhile { !isStop(it) }
+        .map {
+            when (it) {
+                "AUG" -> "Methionine"
+                "UUU", "UUC" -> "Phenylalanine"
+                "UUA", "UUG" -> "Leucine"
+                "UCU", "UCC", "UCA", "UCG" -> "Serine"
+                "UAU", "UAC" -> "Tyrosine"
+                "UGU", "UGC" -> "Cysteine"
+                "UGG" -> "Tryptophan"
+                else -> throw IllegalArgumentException("invalid codon")
+            }
+        }
+}
+
+private fun isStop(codon: String): Boolean {
+    return codon in setOf("UAA", "UAG", "UGA")
 }
