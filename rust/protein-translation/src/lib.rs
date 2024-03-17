3c2b647540bded20656b3a91c6939ecf33bfbf50
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 18:25:18 -0500

    Update protein-translation problem

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,19 +1,43 @@
+use std::collections::HashMap;
+
 pub struct CodonsInfo<'a> {
-    // We fake using 'a here, so the compiler does not complain that
-    // "parameter `'a` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<&'a ()>,
+    map: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> CodonsInfo<'a> {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        todo!("Return the protein name for a '{codon}' codon or None, if codon string is invalid");
+        self.map.get(codon).copied()
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        todo!("Return a list of protein names that correspond to the '{rna}' RNA string or None if the RNA string is invalid");
+        let mut proteins = vec![];
+        let chars: Vec<char> = rna.chars().collect::<Vec<char>>();
+        for chunk in chars.chunks(3) {
+            let codon = chunk.iter().collect::<String>();
+            match self.map.get(&*codon) {
+                None => return None,
+                Some(&protein) => {
+                    if protein == "stop codon" {
+                        break;
+                    }
+                    proteins.push(protein);
+                }
+            }
+        }
+        Some(proteins)
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    todo!("Construct a new CodonsInfo struct from given pairs: {pairs:?}");
+    let mut codons_info = CodonsInfo::new();
+    pairs.iter().for_each(|(codon, protein)| {
+        codons_info.map.insert(codon, protein);
+    });
+    codons_info
 }
