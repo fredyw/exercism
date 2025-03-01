7fce98e034374f579e31e5a19568a934d92d67ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-01 10:35:59 -0600

    Update wordy problem

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,3 +1,58 @@
 pub fn answer(command: &str) -> Option<i32> {
-    todo!("Return the result of the command '{command}' or None, if the command is invalid.");
+    let words: Vec<&str> = command.split(" ").collect();
+    if words[0] != "What" || words[1] != "is" {
+        return None;
+    }
+    let answer = ignore_question_mark(words[2]).parse::<i32>();
+    if answer.is_err() {
+        return None;
+    }
+    let mut answer = answer.unwrap();
+    let mut i = 3;
+    while i < words.len() {
+        match words[i] {
+            "plus" => {
+                if let Ok(n) = ignore_question_mark(words[i + 1]).parse::<i32>() {
+                    answer += n;
+                } else {
+                    return None;
+                }
+                i += 2;
+            }
+            "minus" => {
+                if let Ok(n) = ignore_question_mark(words[i + 1]).parse::<i32>() {
+                    answer -= n;
+                } else {
+                    return None;
+                }
+                i += 2;
+            }
+            "multiplied" => {
+                if let Ok(n) = ignore_question_mark(words[i + 2]).parse::<i32>() {
+                    answer *= n;
+                } else {
+                    return None;
+                }
+                i += 3;
+            }
+            "divided" => {
+                if let Ok(n) = ignore_question_mark(words[i + 2]).parse::<i32>() {
+                    answer /= n;
+                } else {
+                    return None;
+                }
+                i += 3;
+            }
+            _ => return None,
+        }
+    }
+    Some(answer)
+}
+
+fn ignore_question_mark(s: &str) -> &str {
+    if s.ends_with("?") {
+        &s[0..s.len() - 1]
+    } else {
+        s
+    }
 }
