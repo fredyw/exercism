76ca7c9ce925d92a5d2d9c971b5cbef595bf132d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-01 11:32:00 -0600

    Update wordy problem

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -3,50 +3,30 @@ pub fn answer(command: &str) -> Option<i32> {
     if words.len() < 3 || words[0] != "What" || words[1] != "is" {
         return None;
     }
-    let mut answer = if let Ok(n) = words[2].parse::<i32>() {
-        n
-    } else {
-        return None;
-    };
+    let mut answer = words[2].parse::<i32>().ok()?;
     let mut i = 3;
     while i < words.len() {
         match words[i] {
             "plus" => {
-                if let Some(n) = words.get(i + 1).and_then(|w| w.parse::<i32>().ok()) {
-                    answer += n;
-                } else {
-                    return None;
-                }
+                answer += words.get(i + 1).and_then(|w| w.parse::<i32>().ok())?;
                 i += 2;
             }
             "minus" => {
-                if let Some(n) = words.get(i + 1).and_then(|w| w.parse::<i32>().ok()) {
-                    answer -= n;
-                } else {
-                    return None;
-                }
+                answer -= words.get(i + 1).and_then(|w| w.parse::<i32>().ok())?;
                 i += 2;
             }
             "multiplied" => {
                 if i + 2 >= words.len() || words[i + 1] != "by" {
                     return None;
                 }
-                if let Ok(n) = words[i + 2].parse::<i32>() {
-                    answer *= n;
-                } else {
-                    return None;
-                }
+                answer *= words[i + 2].parse::<i32>().ok()?;
                 i += 3;
             }
             "divided" => {
                 if i + 2 >= words.len() || words[i + 1] != "by" {
                     return None;
                 }
-                if let Ok(n) = words[i + 2].parse::<i32>() {
-                    answer /= n;
-                } else {
-                    return None;
-                }
+                answer /= words[i + 2].parse::<i32>().ok()?;
                 i += 3;
             }
             "raised" => {
@@ -57,9 +37,10 @@ pub fn answer(command: &str) -> Option<i32> {
                 {
                     return None;
                 }
-                if let Ok(n) = &words[i + 3][0..words[i + 3].len() - 2].parse::<i32>() {
-                    answer = answer.pow(*n as u32);
-                }
+                let exp = &words[i + 3][0..words[i + 3].len() - 2]
+                    .parse::<i32>()
+                    .ok()?;
+                answer = answer.pow(*exp as u32);
                 i += 5;
             }
             _ => return None,
