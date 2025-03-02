c6c289e1f240301fa3678d9477f684c5246e5fb5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-02 15:17:49 -0600

    Solve sieve problem

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    todo!("Construct a vector of all primes up to {upper_bound}");
+    let mut primes = vec![];
+    let mut visited: Vec<bool> = vec![false; upper_bound as usize + 1];
+    for i in 2..=upper_bound {
+        if visited[i as usize] {
+            continue;
+        }
+        primes.push(i);
+        let mut j = i;
+        while j <= upper_bound {
+            visited[j as usize] = true;
+            j += i;
+        }
+    }
+    primes
 }
