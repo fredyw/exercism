ac2d65fc861ed2ab41a7fa79174bfe97351c4352
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-06 00:15:33 -0500

    Update roman-numerals problem

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -3,8 +3,24 @@ use std::fmt::{Display, Formatter, Result};
 pub struct Roman(u32);
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        todo!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        let mut s = String::new();
+        let mut num = self.0;
+        let mut x = 1;
+        while num > 0 {
+            let n = x * (num % 10);
+            match n {
+                1..5 => todo!(),
+                5..10 => todo!(),
+                10..50 => todo!(),
+                50..100 => todo!(),
+                100..500 => todo!(),
+                _ => todo!(),
+            }
+            num /= 10;
+            x *= 10;
+        }
+        write!(f, "{}", s.chars().rev().collect::<String>())
     }
 }
 
