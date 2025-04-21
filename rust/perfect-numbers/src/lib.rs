bebc8eead93935d003648a818a385c6a4b291722
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-20 20:55:10 -0500

    Solve perfect-numbers problem

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -1,3 +1,6 @@
+use crate::Classification::{Abundant, Deficient, Perfect};
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
@@ -6,5 +9,22 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    todo!("classify {num}");
+    if num == 0 {
+        return None;
+    }
+    let sum: u64 = (1..=num.isqrt())
+        .filter(|n| num % n == 0)
+        .flat_map(|n| {
+            if n == num / n {
+                vec![n]
+            } else {
+                vec![n, num / n]
+            }
+        })
+        .sum();
+    match (sum - num).cmp(&num) {
+        Ordering::Less => Some(Deficient),
+        Ordering::Equal => Some(Perfect),
+        Ordering::Greater => Some(Abundant),
+    }
 }
