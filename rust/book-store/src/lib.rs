84905de2008716c59abae380966a66717e742b37
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-26 01:39:16 -0500

    Update book-store problem

--- rust/book-store/src/lib.rs
+++ rust/book-store/src/lib.rs
@@ -12,82 +12,46 @@ pub fn lowest_price(books: &[u32]) -> u32 {
         n * (100 - discount) * 8
     }
 
-    fn lowest_price<'a>(book_counts: [i32; 5], memo: &mut HashMap<[i32; 5], u32>) -> Option<u32> {
+    fn lowest_price(book_counts: [i32; 5], memo: &mut HashMap<[i32; 5], u32>) -> Option<u32> {
+        fn calculate_price(
+            group: u32,
+            depth: u32,
+            indexes: &mut Vec<usize>,
+            mut book_counts: [i32; 5],
+            memo: &mut HashMap<[i32; 5], u32>,
+            min_price: &mut u32,
+        ) {
+            if depth == 0 {
+                for i in indexes.iter() {
+                    book_counts[i - 1] -= 1;
+                }
+                if let Some(p) = lowest_price(book_counts, memo) {
+                    *min_price = *min_price.min(&mut (p + price(group)));
+                }
+                return;
+            }
+            for i in *indexes.last().unwrap_or(&0)..5 {
+                indexes.push(i + 1);
+                calculate_price(group, depth - 1, indexes, book_counts, memo, min_price);
+                indexes.pop();
+            }
+        }
+
         if book_counts.iter().any(|count| *count < 0) {
             return None;
         }
         if let Some(price) = memo.get(&book_counts) {
             return Some(*price);
         }
-        let mut answer = u32::MAX;
-        for i in 0..5 {
-            let mut new_book_counts = book_counts.clone();
-            new_book_counts[i] -= 1;
-            if let Some(p) = lowest_price(new_book_counts, memo) {
-                answer = answer.min(p + price(1));
-            }
-        }
-        for i in 0..5 {
-            for j in i + 1..5 {
-                let mut new_book_counts = book_counts.clone();
-                new_book_counts[i] -= 1;
-                new_book_counts[j] -= 1;
-                if let Some(p) = lowest_price(new_book_counts, memo) {
-                    answer = answer.min(p + price(2));
-                }
-            }
-        }
-        for i in 0..5 {
-            for j in i + 1..5 {
-                for k in j + 1..5 {
-                    let mut new_book_counts = book_counts.clone();
-                    new_book_counts[i] -= 1;
-                    new_book_counts[j] -= 1;
-                    new_book_counts[k] -= 1;
-                    if let Some(p) = lowest_price(new_book_counts, memo) {
-                        answer = answer.min(p + price(3));
-                    }
-                }
-            }
-        }
-        for i in 0..5 {
-            for j in i + 1..5 {
-                for k in j + 1..5 {
-                    for l in k + 1..5 {
-                        let mut new_book_counts = book_counts.clone();
-                        new_book_counts[i] -= 1;
-                        new_book_counts[j] -= 1;
-                        new_book_counts[k] -= 1;
-                        new_book_counts[l] -= 1;
-                        if let Some(p) = lowest_price(new_book_counts, memo) {
-                            answer = answer.min(p + price(4));
-                        }
-                    }
-                }
-            }
-        }
-        for i in 0..5 {
-            for j in i + 1..5 {
-                for k in j + 1..5 {
-                    for l in k + 1..5 {
-                        for m in l + 1..5 {
-                            let mut new_book_counts = book_counts.clone();
-                            new_book_counts[i] -= 1;
-                            new_book_counts[j] -= 1;
-                            new_book_counts[k] -= 1;
-                            new_book_counts[l] -= 1;
-                            new_book_counts[m] -= 1;
-                            if let Some(p) = lowest_price(new_book_counts, memo) {
-                                answer = answer.min(p + price(5));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        let answer = if answer == u32::MAX { 0 } else { answer };
-        memo.insert(book_counts, answer);
-        Some(answer)
+        let mut min_price = u32::MAX;
+        calculate_price(1, 1, &mut vec![], book_counts, memo, &mut min_price);
+        calculate_price(2, 2, &mut vec![], book_counts, memo, &mut min_price);
+        calculate_price(3, 3, &mut vec![], book_counts, memo, &mut min_price);
+        calculate_price(4, 4, &mut vec![], book_counts, memo, &mut min_price);
+        calculate_price(5, 5, &mut vec![], book_counts, memo, &mut min_price);
+        let min_price = if min_price == u32::MAX { 0 } else { min_price };
+        memo.insert(book_counts.clone(), min_price);
+        Some(min_price)
     }
 
     let mut book_counts = [0, 0, 0, 0, 0];
