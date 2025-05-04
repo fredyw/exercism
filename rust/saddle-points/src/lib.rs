cb9bff59cac240cf75ca75cad72d85d99a8d1316
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-04 12:11:16 -0500

    Solve saddle-points problem

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,30 +1,23 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut rows: Vec<u64> = vec![];
-    let mut cols: Vec<u64> = vec![];
     let num_rows = input.len();
     let num_cols = if num_rows > 0 { input[0].len() } else { 0 };
-    for row in 0..num_rows {
-        let mut max = 0;
-        for col in 0..num_cols {
-            max = max.max(input[row][col]);
-        }
-        rows.push(max);
-    }
-
-    for col in 0..num_cols {
-        let mut min = u64::MAX;
-        for row in 0..num_rows {
-            min = min.min(input[row][col]);
-        }
-        cols.push(min);
-    }
-    let mut answer: Vec<(usize, usize)> = vec![];
-    for row in 0..num_rows {
-        for col in 0..num_cols {
-            if input[row][col] == rows[row] && input[row][col] == cols[col] {
-                answer.push((row, col));
-            }
-        }
-    }
-    answer
+    input
+        .iter()
+        .enumerate()
+        .flat_map(|(row, cols)| {
+            let max_rows: Vec<u64> = (0..num_rows)
+                .filter_map(|row| (0..num_cols).map(|col| input[row][col]).max())
+                .collect();
+            let min_cols: Vec<u64> = (0..num_cols)
+                .filter_map(|col| (0..num_rows).map(|row| input[row][col]).min())
+                .collect();
+            cols.iter().enumerate().filter_map(move |(col, num)| {
+                if *num == max_rows[row] && *num == min_cols[col] {
+                    Some((row, col))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect::<Vec<(usize, usize)>>()
 }
