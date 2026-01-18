a0c0fdf3347e55ac7df5c57b7af7e33345368519
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-18 09:55:50 -0600

    Solve pythagorean-triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,7 +1,21 @@
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    todo!(
-        "Given the sum {sum}, return all possible Pythagorean triplets, which produce the said sum, or an empty HashSet if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c"
-    );
+    let mut triplets = HashSet::new();
+    if sum % 2 != 0 {
+        return triplets;
+    }
+    for a in 1..sum / 3 {
+        // b = (sum^2 - 2 *sum * a) / (2 * sum - 2a)
+        let numerator = sum.pow(2) - 2 * sum * a;
+        let denominator = 2 * sum - 2 * a;
+        if numerator % denominator == 0 {
+            let b = numerator / denominator;
+            if b > a {
+                let c = sum - a - b;
+                triplets.insert([a, b, c]);
+            }
+        }
+    }
+    triplets
 }
